c4d098e5955a6f68501074927713e7f9a6360d22
Xxx678 <2625383606@qq.com>
2019-12-08 23:08:55 +0800

    Update lesson_02_datatype.py

--- python/code/lesson_02_datatype.py
+++ python/code/lesson_02_datatype.py
@@ -20,6 +20,7 @@
 
 
 #=========================================Number数字=======================================
+"""
 #支持int,float,bool,complex
 # a,b,c,d = 2, 1.2, False, 2+3j
 # print(type(a),type(b),type(c),type(d))
@@ -39,6 +40,7 @@
 #print(math.pi,math.e)
 #三角函数
 #print(math.cos(math.pi/2))
+"""
 
 #=========================================String字符串=====================================
 #字符串用'或"来创建
@@ -57,7 +59,7 @@ str2 = "python ii nohtyp!"
 #isalpha,isdigit,isalnum,isspace
 #captalize,title,startswith,endswith,lower,upper,ljust,rjust,center,lstrip,rstrip,strip,join
 #转义字符使用反斜杠转义 记住撇捺 正斜杠/  反斜杠\
-# """ 转义字符	描述
+# 转义字符	描述
 # \(在行尾时)	续行符
 # \\	反斜杠符号
 # \'	单引号
@@ -73,7 +75,7 @@ str2 = "python ii nohtyp!"
 # \oyy	八进制数，yy 代表的字符 例如：\o12 代表换行，其中 o 是字母，不是数字 0
 # \xyy	十六进制数，yy代表的字符 例如：\x0a代表换行
 # \other	其它的字符以普通格式输出
-# """
+
 #字符串运算
 print(
     str1+str2,
@@ -87,6 +89,7 @@ print(
 )
 #字符串格式化输出
 print ("我叫 %s 今年 %d 岁!" % ('小明', 10))
+
 
 """
   符   号	描述
@@ -119,6 +122,7 @@ f-string
 f-string 是 python3.6 之后版本添加的，称之为字面量格式化字符串，是新的格式化字符串的语法。
 之前我们习惯用百分号 (%):
 """
+"""
 str3 = "我就测试一下f-string"
 print(f'hello {str3}')
 
@@ -129,13 +133,59 @@ print(f'{x+1}')   # Python 3.6 输出结果2
 #print(f'{x+1=}')   # Python 3.8 输出结果'x+1=4'
 
 #字符编码 Unicode ASCii
-
+"""
 
 #=========================================List列表==========================================
+'''
+# list1 = ['hello','python',2019,2020]
+# list2 = [1,2,3,4,5,6,7,8,9]
+# print(
+#     list1[0],
+#     list2[3:5]
+# )
+# list2[0] = 'first'
+# print(list2)
+# del list2[5]
+# print(list2)
+# print(
+#     len(list2),
+#     list1 + list2,
+#     list1 *4,
+#     4 in list2,
+# )
+for q in [10,11,12]:
+    print(q)
+list3 = [[1,2,3],[4,5,6],7]
+print(list3[1][0])
+
+#list的方法
+#list3.fuc()增删改查
+'''
 
 
 #=========================================Tuple元组=========================================
-
+"""
+#元组中的元素值是不允许修改
+tuple1 = (1,2,3,4,5)
+tuple2 = (1,)
+tuple3 = (1)
+tuple4 = 1,2,3,'a','b'
+print(type(tuple2),type(tuple3),type(tuple4))
+print(tuple1[0],tuple1[1:2])
+print(tuple1+tuple2)
+#元组中的元素值是不允许删除的，但我们可以使用del语句来删除整个元组
+# print(tuple2)
+# del tuple2
+# print(tuple2)
+print(
+    len(tuple1),
+    tuple2+tuple4,
+    ('hi')*2,
+    2 in tuple1,
+    [i for i in tuple1]
+)
+#tuple1.fuc()
+"""
 
 #=========================================Set集合===========================================
 
@@ -160,6 +210,7 @@ print(f'{x+1}')   # Python 3.6 输出结果2
 is is not	身份运算符
 in not in	成员运算符
 not and or	逻辑运算符
+"""
 """
 #算术运算符
 # a = 4
@@ -190,10 +241,10 @@ not and or	逻辑运算符
 # c = 0
 # print(c)
 # += -= *= /= %= **= //=
-"""python3.8支持
+'''python3.8支持
 if (n := len(a)) > 10:
     print(f"List is too long ({n} elements, expected <= 10)")
-"""
+'''
 #位运算符
 # & | ^异或运算 ~ << >>
 
@@ -204,4 +255,34 @@ if (n := len(a)) > 10:
 #in not in
 
 #身份运算
-#is not is
+# is,not is
+"""
+
+#----------------------------------------列表推导式，字典推导式，集合推导式--------------
+"""
+#1、使用[]生成list
+#基本格式：[表达式 for 变量 in 列表]    或者  [表达式 for 变量 in 列表 if 条件]
+#求(x,y)其中x是0-5之间的偶数，y是0-5之间的奇数组成的元祖列表
+res1=[(x, y) for x in range(5) if x % 2 == 0 for y in range(5) if y % 2 == 1]
+print(res1)
+#2、使用()生成generator
+res2 = (i for i in range(30) if i % 3 is 0)
+print(res2)
+
+#字典推导式
+#大小写key合并
+mcase = {'a': 10, 'b': 34, 'A': 7, 'Z': 3}
+mcase_frequency = {
+    k.lower(): mcase.get(k.lower(), 0) + mcase.get(k.upper(), 0)
+    for k in mcase.keys()
+    if k.lower() in ['a','b']
+}
+print(mcase_frequency)
+#快速跟换key和value
+res4 = {u:v for v,u in mcase.items()}
+print(res4)
+
+#集合推导式
+squared = {x**2 for x in [1, 1, 2]}
+print(squared)
+"""
